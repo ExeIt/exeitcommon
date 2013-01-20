8fb065191fb96986f42094d83447ed8d8eb1341b
Paul <Paul@81bc1760-0820-cd4b-86e7-401607c5deec>
2013-01-20 18:01:20 +0000

    Initial version of common utilities solution

---
+++ common/utilities/trunk/EnumUtils.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EXE_IT.Common.Utilities
+{
+    /// <summary>
+    /// Utility functions for Enums
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public static class EnumUtil<T>
+    {
+        /// <summary>
+        /// Parses the specified value into an Enum.
+        /// </summary>
+        /// <param name="value">The value.</param>
+        /// <returns></returns>
+        public static T Parse(string value)
+        {
+            return (T)Enum.Parse(typeof(T), value);
+        }
+
+        /// <summary>
+        /// Parses the specified value.
+        /// </summary>
+        /// <param name="value">The value.</param>
+        /// <param name="ignoreCase">if set to <c>true</c> [ignore case].</param>
+        /// <returns></returns>
+        public static T Parse(string value, bool ignoreCase)
+        {
+            return (T)Enum.Parse(typeof(T), value, ignoreCase);
+        }
+
+        /// <summary>
+        /// Gets a list of all the values in an enum.
+        /// </summary>
+        /// <returns></returns>
+        public static IList<T> GetValues()
+        {
+            IList<T> list = new List<T>();
+
+            foreach (object value in Enum.GetValues(typeof(T)))
+                list.Add((T)value);
+
+            return list;
+        }
+    }
+}
---
+++ common/utilities/trunk/Obfuscator.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Text;
+
+namespace EXE_IT.Common.Utilities
+{
+    public static class Obfuscator
+    {
+        public static string Obfuscate(string text)
+        {
+            var sb = new StringBuilder("_");
+
+            if (text != null)
+            {
+                foreach (char c in text)
+                {
+                    string ac = ((int)c).ToString("X").ToLower();
+                    sb.Append(ac);
+                }
+            }
+
+            return sb.ToString();
+        }
+
+        public static string UnObfuscate(string text)
+        {
+            if (text == null) throw new ArgumentNullException("text");
+
+            if (text == "_")
+                return null;
+
+            if ((!text.StartsWith("_") || ((text.Length % 2) == 0)))
+            {
+                throw new FormatException("The obfuscated text was not in the correct format.");
+            }
+            else
+            {
+                try
+                {
+                    var sb = new StringBuilder();
+
+                    for (int i = 1; i < text.Length; i = i + 2)
+                    {
+                        string acs = text.Substring(i, 2);
+                        int ac = Convert.ToInt32("0x" + acs, 16);
+                        sb.Append((char)ac);
+                    }
+
+                    return sb.ToString();
+                }
+                catch (Exception ex)
+                {
+                    throw new FormatException(String.Format(
+                        "Error unobfuscating '{0}'. If value is from query string, user may be attempting hack.", text), ex);
+                }
+            }
+
+        }
+    }
+}
