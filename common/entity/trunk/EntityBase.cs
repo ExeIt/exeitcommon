6b2f662d0b02169f593f04f7e721a491ef90d0ff
Paul <Paul@81bc1760-0820-cd4b-86e7-401607c5deec>
2013-01-20 17:57:27 +0000

    Initial common entity project

---
+++ common/entity/trunk/EntityBase.cs
@@ -0,0 +1,285 @@
+﻿using System;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using EXE_IT.Common.Data;
+
+namespace EXE_IT.Common.Entity
+{
+    [Serializable]
+    public class EntityBase
+    {
+        /// <summary>
+        /// The name of the ID parameter in the SqlCommand.Parameters collection
+        /// </summary>
+        protected const string idParamName = "id";
+
+        #region Constructors
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public EntityBase()
+        {
+            LastUpdateDt = DateTime.MinValue;
+            LastUpdatedBy = String.Empty;
+            Id = 0;
+        }
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="reader">Data reader containing a row holding data on this generic table</param>
+        protected EntityBase(IDataReader reader)
+        {
+            LastUpdatedBy = String.Empty;
+
+            Id = SqlDataReaderUtil.GetInt32(reader, reader.GetOrdinal("ID"), 0);
+
+            try
+            {
+                if (!Convert.IsDBNull(reader.GetOrdinal("LastUpdatedBY")))
+                    LastUpdatedBy = SqlDataReaderUtil.GetString(reader, reader.GetOrdinal("LastUpdatedBY"));        
+            }
+            catch {}
+
+            try
+            {
+                if (!Convert.IsDBNull(reader.GetOrdinal("LastUpdateDT")))
+                    LastUpdateDt = SqlDataReaderUtil.GetDateTime(reader, reader.GetOrdinal("LastUpdateDT"), DateTime.MaxValue);
+            }
+            catch {}
+
+
+        }
+
+        #endregion
+
+        #region Properties
+
+        public int Id { get; set; }
+
+        public string LastUpdatedBy { get; set; }
+
+        public DateTime LastUpdateDt { get; set; }
+
+        #endregion
+
+        #region Protected Methods
+
+        /// <summary>
+        /// Add all common parameters to the SQL command ready for an update command
+        /// </summary>
+        /// <param name="command">Command to add parameters to</param>
+        /// <param name="updateSp">Stored procedure to call if updating rather than creating</param>
+        protected void AddCommonSqlUpdateParameters(SqlCommand command, string updateSp)
+        {
+            SqlParameter param = command.Parameters.Add(idParamName, SqlDbType.Int);
+            param.Value = Id;
+            param = command.Parameters.Add("LastUpdatedBY", SqlDbType.VarChar, 50);
+            param.Value = LastUpdatedBy;
+
+            // define output param if creating
+            if (Id == 0)
+            {
+                command.Parameters[idParamName].Direction = ParameterDirection.Output;
+            }
+            else
+            {
+                // we're actually doing an update not a create
+                command.CommandText = updateSp;
+            }
+        }
+
+        /// <summary>
+        /// Update this entity on the database
+        /// </summary>
+        /// <param name="command">Populated Command object</param>
+        protected void UpdateEntity(SqlCommand command)
+        {
+            Exception error = null;
+
+            // start transaction
+            command.Transaction = command.Connection.BeginTransaction();
+
+            // execute
+            try
+            {
+                command.ExecuteNonQuery();
+
+                // commit change
+                command.Transaction.Commit();
+
+                // capture ID of created entity
+                if (Id == 0)
+                    Id = (int)command.Parameters[idParamName].Value;
+            }
+            catch (SqlException ex)
+            {
+                // abort transaction
+                command.Transaction.Rollback();
+
+                // remember
+                error = ex;
+            }
+            finally
+            {
+                // close connection
+                command.Connection.Close();
+            }
+
+            // throw exception if an error occurred
+            if (error != null)
+                throw error;
+        }
+
+        /// <summary>
+        /// Update this entity on the database but do NOT end the transaction or CLOSE the connection
+        /// </summary>
+        /// <param name="command">Populated Command object</param>
+        protected void UpdateInsideTransaction(SqlCommand command)
+        {
+            Exception error = null;
+
+            // execute
+            try
+            {
+                command.ExecuteNonQuery();
+
+                // capture ID of created entity
+                if (Id == 0)
+                    Id = (int)command.Parameters[idParamName].Value;
+            }
+            catch (SqlException ex)
+            {
+                // remember
+                error = ex;
+            }
+            finally { }
+
+            // throw exception if an error occurred
+            if (error != null)
+                throw error;
+        }
+
+        /// <summary>
+        /// Update this entity on the database and CLOSE the connection - leave the transaction alone
+        /// as this will be dealt with in the business logic using scope
+        /// </summary>
+        /// <param name="command">Populated Command object</param>
+        protected void UpdateAndCloseConnection(SqlCommand command)
+        {
+            Exception error = null;
+
+            // execute
+            try
+            {
+                command.ExecuteNonQuery();
+
+                // capture ID of created entity
+                if (Id == 0)
+                    Id = (int)command.Parameters[idParamName].Value;
+            }
+            catch (SqlException ex)
+            {
+                // remember
+                error = ex;
+            }
+            finally
+            {
+                // close connection
+                command.Connection.Close();
+            }
+
+            // throw exception if an error occurred
+            if (error != null)
+                throw error;
+        }
+
+        #endregion
+
+        #region Protected Static methods for Database Access
+
+        /// <summary>
+        /// Get an open connection to the database
+        /// </summary>
+        /// <returns></returns>
+        public static SqlConnection GetConnection()
+        {
+            var connectionString = ConfigurationManager.ConnectionStrings["ConnectionString"];
+            var con = new SqlConnection(connectionString.ConnectionString);
+            con.Open();
+            return con;
+        }
+
+        /// <summary>
+        /// Load an entity by its ID
+        /// </summary>
+        /// <param name="storedProc">Stored Proc to use to load entity</param>
+        /// <param name="id">ID to load</param>
+        /// <returns>DataReader positioned at first row</returns>
+        /// <exception cref="ArgumentOutOfRangeException">If the given ID is not a valid ID for the entity type</exception>
+        protected static SqlDataReader LoadById(string storedProc, int id)
+        {
+            SqlCommand command = GetSpCommand(storedProc);
+            SqlDataReader reader = null;
+
+            // define parameters
+            SqlParameter param = command.Parameters.Add("ID", SqlDbType.Int);
+            param.Value = id;
+
+            // execute
+            try
+            {
+                reader = command.ExecuteReader(CommandBehavior.CloseConnection);
+
+                if (reader.HasRows)
+                {
+                    // advance to row 1
+                    reader.Read();
+                }
+                else
+                {
+                    // close connection
+                    command.Connection.Close();
+
+                    // no rows : invalid ID
+                    throw new ArgumentOutOfRangeException("ID", id, "No entity exists for this ID");
+                }
+            }
+            catch (SqlException)
+            {
+                // close connection
+                command.Connection.Close();
+
+                // should do something here
+                throw;
+            }
+
+            // return reader
+            return reader;
+        }
+
+        #endregion
+
+        #region Internal Static methods for Database Access
+
+        /// <summary>
+        /// Get a command object ready to execute a stored procudure
+        /// </summary>
+        /// <remarks>
+        /// An open connection is already associated with the command
+        /// </remarks>
+        /// <param name="storedProcName">Name of stored proc to be executed</param>
+        /// <returns>Sql Command</returns>
+        public static SqlCommand GetSpCommand(string storedProcName)
+        {
+            var command = new SqlCommand(storedProcName, GetConnection());
+            command.CommandType = CommandType.StoredProcedure;
+
+            return command;
+        }
+
+        #endregion
+    }
+}
---
+++ common/entity/trunk/MenuAccess.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace EXE_IT.Common.Entity
+{
+    public class MenuAccess : EntityBase
+    {
+        #region Constructors
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public MenuAccess()
+        {
+            Css = string.Empty;
+            DisplayName = string.Empty;
+            Name = string.Empty;
+            PageUrl = string.Empty;
+            ContentType = 0;
+            MenuType = 0;
+            MenuAccessLevel = 0;
+            SiteAccessLevel = 0;
+        }
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="reader">Data reader containing a row holding data on this MenuAccess item</param>
+        public MenuAccess(IDataReader reader) : base(reader)
+        {
+            Css = string.Empty;
+            DisplayName = string.Empty;
+            Name = string.Empty;
+            PageUrl = string.Empty;
+            ContentType = 0;
+            MenuType = 0;
+            MenuAccessLevel = 0;
+            SiteAccessLevel = 0;
+            int column = reader.GetOrdinal("SiteAccess");
+            if (!reader.IsDBNull(column))
+                SiteAccessLevel = reader.GetInt32(column);
+
+            column = reader.GetOrdinal("MenuAccess");
+            if (!reader.IsDBNull(column))
+                MenuAccessLevel = reader.GetInt32(column);
+
+            column = reader.GetOrdinal("MenuType");
+            if (!reader.IsDBNull(column))
+                MenuType = reader.GetInt32(column);
+
+            column = reader.GetOrdinal("ContentType");
+            if (!reader.IsDBNull(column))
+                ContentType = reader.GetInt32(column);
+
+            column = reader.GetOrdinal("Name");
+            if (!reader.IsDBNull(column))
+                Name = reader.GetString(column);
+
+            column = reader.GetOrdinal("DisplayName");
+            if (!reader.IsDBNull(column))
+                DisplayName = reader.GetString(column);
+
+            column = reader.GetOrdinal("PageURL");
+            if (!reader.IsDBNull(column))
+                PageUrl = reader.GetString(column);
+        }
+
+        #endregion
+
+        #region Properties
+
+        public int SiteAccessLevel { get; set; }
+
+        public int MenuAccessLevel { get; set; }
+
+        public int MenuType { get; set; }
+
+        public int ContentType { get; set; }
+
+        public string PageUrl { get; set; }
+
+        public string Name { get; set; }
+
+        public string DisplayName { get; set; }
+
+        public string Css { get; set; }
+
+        #endregion
+
+        #region Static methods
+
+        /// <summary>
+        /// Loads all menu items appropriate to the rights of the user
+        /// </summary>
+        /// <returns>Dictionary keyed on ID</returns>
+        public static Dictionary<int, MenuAccess> LoadAll(int siteAccess, int menuAccess, int menuType)
+        {
+            var result = new Dictionary<int, MenuAccess>();
+
+            // define command
+            SqlCommand command = GetSpCommand("CP_Menu_Load");
+
+            SqlParameter param = command.Parameters.Add("UserSiteAccess", SqlDbType.Int);
+            param.Value = siteAccess;
+            param = command.Parameters.Add("UserMenuAccess", SqlDbType.Int);
+            param.Value = menuAccess;
+            param = command.Parameters.Add("MenuType", SqlDbType.Int);
+            param.Value = menuType;
+
+            // execute
+            try
+            {
+                SqlDataReader reader = command.ExecuteReader();
+
+                if (reader == null) return result;
+
+                while (reader.Read())
+                {
+                    var obj = new MenuAccess(reader);
+                    result.Add(obj.Id, obj);
+                }
+            }
+            catch (SqlException)
+            {
+                throw;
+            }
+            finally
+            {
+                command.Dispose();
+                command.Connection.Close();
+            }
+
+            return result;
+        }
+
+        #endregion
+    }
+}
