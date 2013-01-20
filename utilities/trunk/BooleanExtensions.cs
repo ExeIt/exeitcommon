4ba29a6eea7346bf1495d2f91e50886573582dbb
Paul <pjzbrown@users.noreply.github.com>
2013-01-20 18:01:20 +0000

    Initial version of common utilities solution

---
+++ utilities/trunk/BooleanExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EXE_IT.Common.Utilities
+{
+    public static class BooleanExtensions
+    {
+        /// <summary>
+        /// Gets the java script value.
+        /// </summary>
+        /// <param name="value">if set to <c>true</c> [value].</param>
+        /// <returns></returns>
+        public static string GetJavaScriptValue(this bool value)
+        {
+            return value ? "true" : "false";
+        }
+    }
+}
